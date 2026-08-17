135ee55f5839fbba406377b4cbf2f9f58b838235
skyluxejourney <skyluxejourney@gmail.com>
2026-08-17 03:10:22 -0700

    changed the hero

--- app/airlines/[slug]/components/AirlineHero.tsx
+++ app/airlines/[slug]/components/AirlineHero.tsx
@@ -32,7 +32,7 @@ export default function AirlineHero({ airline }: AirlineHeroProps) {
           priority
           quality={100}
         />
-        <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/70 to-black/80" />
+        <div className="absolute inset-0 bg-gradient-to-b from-black/40 via-black/30 to-black/20" />
         <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-black/40" />
         <div className="absolute inset-0 bg-black/30" />
       </div>
